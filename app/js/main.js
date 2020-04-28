419059d0e45cbcfcf09000c1cfc8ab9f07c936b6
Krystyna Koryagina <krystyna.koryagina@gmail.com>
2020-04-28 21:05:34 +0300

    finished header section, mobile menu, add js for burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function() {
+	// Mobile Menu
+	$('.burger').on('click', function() {
+		var menu = $('.header__menu');
 
+		$(this).toggleClass('burger--active');
+
+		if (menu.hasClass('header__menu--active')) {
+			menu.removeClass('header__menu--active')
+					.addClass('header__menu--hide');
+		} else {
+			menu.removeClass('header__menu--hide')
+					.addClass('header__menu--active');
+		}
+	});
 });
